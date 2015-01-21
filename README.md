eeb54d2b845c78fdaae5227694b474ff9a89aeca
Cedric TRANG <cedric.trang@gmail.com>
2015-01-21 16:08:29 +0800

    Project cleaned

--- README.md
+++ README.md
@@ -2,4 +2,12 @@ Project_HTML5Benchmark
 ======================
 To set the local server please follow the steps from the below website:
 http://www.mobitechie.com/android-2/how-to-access-localhost-on-android-over-wifi/
+Download the project and put it on C:\wamp\www
 Don' forget to create the database called dataset and load the data from data/dataset.sql
+	To create the database :
+		use your bowser and tape localhost. 
+		Then click on the phpmyadmin. 
+		By default the user is "root" and there is no password
+		Click on Database
+I recommend to use Chrome to benefit all the features that HTML5 propose.
+Also some part of the project will need the VPN according to the country where you are (Geolocation and Video).
--- js/directives.js
+++ js/directives.js
@@ -9,24 +9,6 @@ angular.module('myApp.directives', []).
       elm.text(version);
     };
   }])
-  /*.directive('newpage',['$animate','$document', function($animate,$document){
-    return function(scope, element, attrs){
-        element.on('click',function(){
-
-            var item = angular.element(document.getElementById('scroll'));
-            if(item.hasClass('animate-left')){
-                $animate.removeClass(item, 'animate-left');
-                console.log(item.hasClass('animate-left'));
-                $animate.addClass(item, 'animate-right');
-                //console.log('done1');
-            }else{
-                $animate.removeClass(item, 'animate-right');
-                $animate.addClass(item, 'animate-left');
-                //console.log('done2');
-            }
-        });
-    };
-  }])*/
   .directive('draggable',['$document', function($document){
         return function(scope, element, attr) {
             var startX = 0, startY = 0, x = 0, y = 0;
@@ -61,44 +43,3 @@ angular.module('myApp.directives', []).
         };
     }]);
 
-/*Geolocation*/
-  /*.directive('lazyLoad', ['$window', '$q', function ($window, $q) {
-        function load_script() {
-            var s = document.createElement('script'); // use global document since Angular's $document is weak
-            s.src = 'https://maps.googleapis.com/maps/api/js?sensor=false&callback=initialize';
-            document.body.appendChild(s);
-        }
-        function lazyLoadApi(key) {
-            var deferred = $q.defer();
-            $window.initialize = function () {
-                deferred.resolve();
-            };
-            // thanks to Emil Stenström: http://friendlybit.com/js/lazy-loading-asyncronous-javascript/
-            if ($window.attachEvent) {  
-                $window.attachEvent('onload', load_script); 
-            } else {
-                $window.addEventListener('load', load_script, false);
-            }
-            return deferred.promise;
-        }
-        return {
-            restrict: 'E',
-            link: function (scope, element, attrs) { // function content is optional
-            // in this example, it shows how and when the promises are resolved
-                if ($window.google && $window.google.maps) {
-                    console.log('gmaps already loaded');
-                } else {
-                    lazyLoadApi().then(function () {
-                        console.log('promise resolved');
-                        if ($window.google && $window.google.maps) {
-                            console.log('gmaps loaded');
-                        } else {
-                            console.log('gmaps not loaded');
-                        }
-                    }, function () {
-                        console.log('promise rejected');
-                    });
-                }
-            }
-        };
-   	}]);*/
